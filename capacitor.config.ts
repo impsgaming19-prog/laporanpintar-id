d7dd75dfbe8a42e5f717fa5e1155a9bc39a03964
Imps <impsgaming19@gmail.com>
2026-08-30 16:13:12 +0700

    Add capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,1 +1,17 @@
-{}
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.laporankuyoks.app',
+  appName: 'LaporanPintarID',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ['badge', 'sound', 'alert'],
+    }
+  }
+};
+
+export default config;
